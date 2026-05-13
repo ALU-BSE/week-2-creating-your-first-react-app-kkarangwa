aa2d0a79687a8df3e033490db501957261f59739
Kethia Karangwa <Kethia@TAG.DC>
2026-05-13 15:02:28 +0200

    React Typescript assignment

---
+++ src/MyComponent.tsx
@@ -0,0 +1,11 @@
+import './MyComponent.css';
+function MyComponent() {
+  return (
+    <div className="my-component">
+      <h1>My First React + TypeScript App</h1>
+      <p>A simple demonstration of React components and TypeScript.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
